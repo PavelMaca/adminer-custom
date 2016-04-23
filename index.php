b98858c4790f4c453c3938bfa6493f97961b40f7
Pavel Máca <maca.pavel@gmail.com>
2016-04-23 23:14:46 +0200

    move AdminerTheme files to /static folder & replace images and font with base64 encoded version

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
 
+if (empty($_GET['file'])) {
+    ob_start(function($s) {
+        return preg_replace_callback('#(<(link|script)\s[^>]*(href|src)=")(adminer\.css|static/.+)"#U', function($m) {
+            return $m[1] . '?file=' . urlencode($m[4]) . '"';
+        }, $s);
+    }, 4096);
+} elseif (preg_match('#^(adminer|static(/\w[\w.-]*)+)\.(\w+)\z#', $_GET['file'], $m)) {
+    if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+        header('HTTP/1.1 304 Not Modified');
+        exit;
+    }
+    header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime('1 month')) . ' GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+    $types = array('css' => 'text/css', 'js' => 'text/javascript', 'gif' => 'image/gif', 'png' => 'image/png');
+    if (isset($types[$m[3]])) {
+        header('Content-Type: ' . $types[$m[3]]);
+    }
+    @readfile(__DIR__ . '/' . $_GET['file']);
+    exit;
+}
+
+
 function adminer_object() {
     // required to run any plugin
-    include_once "./plugins/plugin.php";
-    
+    include_once dirname(__FILE__) . '/plugins/plugin.php';
+
     // autoloader
-    foreach (glob("plugins/*.php") as $filename) {
-        include_once "./$filename";
+    foreach (glob(dirname(__FILE__) . '/plugins/*.php') as $filename) {
+        include_once $filename;
     }
     
     $plugins = [
@@ -35,4 +57,4 @@ function adminer_object() {
 }
 
 // include original Adminer or Adminer Editor
-include "./adminer.php";
+include __DIR__ . "./adminer.php";
